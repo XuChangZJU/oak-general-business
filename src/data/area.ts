32b2429e35e5e9933e3c60eead4230939bd8ac4b
Xc <chinawraith@163.com>
2022-04-12 19:51:06 +0800

    编译优化

--- src/data/area.ts
+++ src/data/area.ts
@@ -1,7 +1,7 @@
-import area2 from './area.json';
+// import area2 from './area.json';
 import area_debug from './area-debug.json';
 
-const area = process.env.NODE_ENV === 'production' ? area2 : area_debug;
+const area = area_debug;
 export {
     area,
 };
