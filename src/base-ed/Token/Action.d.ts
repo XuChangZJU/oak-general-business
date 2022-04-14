a40d37ded71440262e50f7db204d3736b6311715
Xc@centOs <chinawraith@163.com>
2022-04-14 17:27:00 +0800

    删除了多余的文件

--- src/base-ed/Token/Action.d.ts
+++
@@ -1,4 +0,0 @@
-import { AbleAction } from "oak-domain/lib/actions/action";
-import { GenericAction } from "oak-domain/lib/actions/action";
-export declare type ParticularAction = AbleAction;
-export declare type Action = GenericAction | ParticularAction;
--- src/base-ed/User/Action.d.ts
+++
@@ -1,8 +0,0 @@
-import { GenericAction } from "oak-domain/lib/actions/action";
-declare type IdAction = 'verify' | 'accept' | 'reject';
-export declare type IdState = 'unverified' | 'verified' | 'verifying';
-declare type UserAction = 'activate' | 'disable' | 'enable' | 'mergeTo' | 'mergeFrom';
-export declare type UserState = 'shadow' | 'normal' | 'disabled' | 'merged';
-export declare type ParticularAction = UserAction | IdAction;
-export declare type Action = GenericAction | ParticularAction;
-export {};
