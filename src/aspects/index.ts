44c755ac5b700af9e13dcc82aab1708b84e3574e
Xc <chinawraith@163.com>
2022-04-15 19:58:59 +0800

    将crud的aspect移到了base里

--- src/aspects/index.ts
+++ src/aspects/index.ts
@@ -1,13 +1,10 @@
 import { EntityDict } from 'oak-domain/lib/types';
 import { EntityDict as BaseEntityDict } from '../base-ed/EntityDict';
 import { loginByPassword, loginMp, /* AspectDict as TokenAD  */} from './token';
-import { operate, select, /* AspectDict as CrudAD */ } from './crud';
 
 const aspectDict = {
     loginByPassword,
     loginMp,
-    operate,
-    select,
 };
 
 export default aspectDict;
