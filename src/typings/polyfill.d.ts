005ac81300d895c745755870b204130701f36f2b
梁朝伟 <2211960668@qq.com>
2022-04-22 13:40:54 +0800

    将oakPage和oakComponent加入到全局中

--- src/typings/polyfill.d.ts
+++ src/typings/polyfill.d.ts
@@ -1,8 +1,9 @@
-// declare const getRandomValues: (length: number) => Promise<Uint8Array>;
-
 import { MakeOakComponent, MakeOakPage } from 'oak-frontend-base';
 import { EntityDict } from 'oak-app-domain/EntityDict';
 import { RuntimeContext } from '../RuntimeContext';
 import aspectDict from '../aspects';
-declare const OakPage: MakeOakPage<EntityDict, RuntimeContext<EntityDict>, typeof aspectDict, {}>;
-declare const OakComponent: MakeOakComponent<EntityDict, RuntimeContext<EntityDict>, typeof aspectDict, {}>;
+declare global {
+    const OakPage: MakeOakPage<EntityDict, RuntimeContext<EntityDict>, typeof aspectDict, {}>;
+    const OakComponent: MakeOakComponent<EntityDict, RuntimeContext<EntityDict>, typeof aspectDict, {}>;
+}
+export {}
