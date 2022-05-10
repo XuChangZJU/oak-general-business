766e06dbb50bc45e580f8b861a3eeb6d0aa470fd
Xc <chinawraith@163.com>
2022-05-10 09:26:02 +0800

    merge

--- lib/utils/extraFile.d.ts
+++ lib/utils/extraFile.d.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import { OpSchema as ExtraFile } from '../base-app-domain/ExtraFile/Schema';
-=======
 import { OpSchema as ExtraFile } from 'oak-app-domain/ExtraFile/Schema';
->>>>>>> 29abd56e063b345c3d535d45f7926ddc2d0e7f9f
 export declare function composeFileUrl(extraFile: ExtraFile): string;
 export declare function decomposeFileUrl(url: string): Pick<ExtraFile, 'bucket' | 'filename' | 'origin' | 'type' | 'extra1'>;
