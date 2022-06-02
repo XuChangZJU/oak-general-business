013f2507fc47fa5a44eff062fb7e7d44dd280622
Xc <chinawraith@163.com>
2022-06-02 14:09:23 +0800

    watcher

---
+++ lib/watchers/index.d.ts
@@ -0,0 +1,5 @@
+import { EntityDict } from 'oak-app-domain';
+import { Watcher } from 'oak-domain/lib/types';
+import { GeneralRuntimeContext } from '../RuntimeContext';
+declare const _default: Watcher<EntityDict, keyof EntityDict, GeneralRuntimeContext<EntityDict>>[];
+export default _default;
