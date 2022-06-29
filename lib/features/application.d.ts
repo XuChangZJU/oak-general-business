4ac0693ea4cedf77fb9a746bef291f5d6ef3018e
Xc@centOs <chinawraith@163.com>
2022-06-29 13:24:23 +0800

    build

--- lib/features/application.d.ts
+++ lib/features/application.d.ts
@@ -12,7 +12,7 @@ export declare class Application<ED extends EntityDict, Cxt extends GeneralRunti
     private cache;
     constructor(aspectWrapper: AspectWrapper<ED, Cxt, AD>, applicationId: string, cache: Cache<ED, Cxt, AD & CommonAspectDict<ED, Cxt>>);
     private refresh;
-    getApplication(): SelectRowShape<ED["application"]["Schema"], {
+    getApplication(): Promise<SelectRowShape<ED["application"]["Schema"], {
         id: 1;
         name: 1;
         config: 1;
@@ -23,6 +23,6 @@ export declare class Application<ED extends EntityDict, Cxt extends GeneralRunti
             name: 1;
             config: 1;
         };
-    }>;
+    }>>;
     getApplicationId(): string;
 }
