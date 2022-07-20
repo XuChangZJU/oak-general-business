218ef3d2b6e3448f35c3d400f154191caa16a8e9
wkj <278599135@qq.com>
2022-07-20 19:01:26 +0800

    去掉不要的代码

--- app/components/UI/cell/index.ts
+++
@@ -1,9 +0,0 @@
-Component({
-    externalClasses: ['g-class', 'g-label-class', 'g-content-class'],
-    properties: {
-        label: {
-            type: String,
-            value: '',
-        },
-    },
-});
