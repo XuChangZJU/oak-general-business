f9d238e89b13af4bca4e0ba3ff83284ead8a4911
梁朝伟 <2211960668@qq.com>
2022-09-27 20:10:37 +0800

    fix

--- src/pages/userRelation/upsert/web.pc.tsx
+++ src/pages/userRelation/upsert/web.pc.tsx
@@ -8,7 +8,7 @@ import {
     Button,
 } from 'tdesign-react';
 const { FormItem } = Form;
-
+import Style from './web.module.less';
 
 export default function render(this: any) {
     const { relations, entity } = this.props;
@@ -18,137 +18,143 @@ export default function render(this: any) {
             ? relations
             : relations && JSON.parse(relations);
     return (
-        <div>
-            <Row gutter={16}>
-                <Col span={4}>
-                    <Form
-                        colon={false}
-                        labelAlign="right"
-                        labelWidth="100px"
-                        layout="vertical"
-                        preventSubmitDefault
-                        resetType="empty"
-                        showErrorMessage
-                        submitWithWarningMessage={false}
-                    >
-                        <FormItem
-                            label="姓名"
-                            name="name"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '姓名不能为空',
-                                    type: 'error',
-                                },
-                            ]}
+        <div className={Style.pageWithPadding}>
+            <div className={Style.formContainer}>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Form
+                            colon={false}
+                            labelAlign="right"
+                            labelWidth="100px"
+                            layout="vertical"
+                            preventSubmitDefault
+                            resetType="empty"
+                            showErrorMessage
+                            submitWithWarningMessage={false}
                         >
-                            <Input
-                                onChange={(value, context) => {
-                                    this.setUpdateData('name', value);
-                                }}
-                                value={name}
-                                align="left"
-                                placeholder="请输入内容"
-                                type="text"
-                            />
-                        </FormItem>
-                        <FormItem
-                            label="手机号码"
-                            name="mobile"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '手机号不能为空',
-                                    type: 'error',
-                                },
-                                {
-                                    min: 11,
-                                    message: '请输入11位手机号',
-                                    type: 'error',
-                                },
-                                {
-                                    max: 11,
-                                    message: '请输入11位手机号',
-                                    type: 'error',
-                                },
-                            ]}
-                        >
-                            <Input
-                                onChange={(value, context) => {
-                                    this.setUpdateData(
-                                        'mobile$user.0.mobile',
-                                        value
-                                    );
-                                }}
-                                value={mobile}
-                                align="left"
-                                placeholder="请输入内容"
-                                size="medium"
-                                type="text"
-                            />
-                        </FormItem>
-                        <FormItem
-                            initialData="123456"
-                            label="密码"
-                            name="mobile"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '密码不能为空',
-                                    type: 'error',
-                                },
-                            ]}
-                        >
-                            <Input
-                                value={password}
-                                align="left"
-                                placeholder="请输入内容"
-                                showClearIconOnEmpty={false}
-                                size="medium"
-                                status="default"
-                                type="text"
-                            />
-                        </FormItem>
-                        <FormItem
-                            label="权限"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: '请至少选择一个权限',
-                                    type: 'error',
-                                },
-                            ]}
-                        >
-                            <Checkbox.Group
-                                value={relationArr}
-                                onChange={(value) => {
-                                    this.setRelationValue(value);
-                                }}
-                                options={relationArr2.map((ele) => ({
-                                    value: ele,
-                                    label:
-                                        (this.t &&
-                                            this.t(entity + ':r.' + ele)) ||
-                                        ele,
-                                }))}
-                            ></Checkbox.Group>
-                        </FormItem>
-                        <FormItem style={{ marginLeft: 100 }}>
-                            <Button
-                                theme="primary"
-                                type="submit"
-                                style={{ marginRight: 10 }}
-                                onClick={() => {
-                                    this.onConfirm();
-                                }}
+                            <FormItem
+                                label="姓名"
+                                name="name"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: '姓名不能为空',
+                                        type: 'error',
+                                    },
+                                ]}
                             >
-                                提交
-                            </Button>
-                            <Button type="reset">重置</Button>
-                        </FormItem>
-                    </Form>
-                </Col>
-            </Row>
+                                <Input
+                                    onChange={(value, context) => {
+                                        this.setUpdateData('name', value);
+                                    }}
+                                    value={name}
+                                    align="left"
+                                    placeholder="请输入内容"
+                                    type="text"
+                                />
+                            </FormItem>
+                            <FormItem
+                                label="手机号码"
+                                name="mobile"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: '手机号不能为空',
+                                        type: 'error',
+                                    },
+                                    {
+                                        min: 11,
+                                        message: '请输入11位手机号',
+                                        type: 'error',
+                                    },
+                                    {
+                                        max: 11,
+                                        message: '请输入11位手机号',
+                                        type: 'error',
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    onChange={(value, context) => {
+                                        this.setUpdateData(
+                                            'mobile$user.0.mobile',
+                                            value
+                                        );
+                                    }}
+                                    maxlength={11}
+                                    value={mobile}
+                                    align="left"
+                                    placeholder="请输入内容"
+                                    size="medium"
+                                    type="text"
+                                />
+                            </FormItem>
+                            <FormItem
+                                initialData="12345678"
+                                label="密码"
+                                name="mobile"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: '密码不能为空',
+                                        type: 'error',
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    value={password}
+                                    onChange={(value) => {
+                                        this.setUpdateData('password', value);
+                                    }}
+                                    align="left"
+                                    placeholder="不少于八位"
+                                    showClearIconOnEmpty={false}
+                                    size="medium"
+                                    status="default"
+                                    type="text"
+                                />
+                            </FormItem>
+                            <FormItem
+                                label="权限"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: '请至少选择一个权限',
+                                        type: 'error',
+                                    },
+                                ]}
+                            >
+                                <Checkbox.Group
+                                    value={relationArr}
+                                    onChange={(value) => {
+                                        this.setRelationValue(value);
+                                    }}
+                                    options={relationArr2.map((ele) => ({
+                                        value: ele,
+                                        label:
+                                            (this.t &&
+                                                this.t(entity + ':r.' + ele)) ||
+                                            ele,
+                                    }))}
+                                ></Checkbox.Group>
+                            </FormItem>
+                            <FormItem style={{ marginLeft: 100 }}>
+                                <Button
+                                    theme="primary"
+                                    type="submit"
+                                    style={{ marginRight: 10 }}
+                                    onClick={() => {
+                                        this.onConfirm();
+                                    }}
+                                >
+                                    提交
+                                </Button>
+                                <Button theme="default" type="reset">重置</Button>
+                            </FormItem>
+                        </Form>
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 }
