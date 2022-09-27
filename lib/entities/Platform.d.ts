80937742dca033a1ad15b60847e010d9a7390173
wkj <278599135@qq.com>
2022-09-27 10:54:23 +0800

    临时 过渡

--- lib/entities/Platform.d.ts
+++ lib/entities/Platform.d.ts
@@ -1,6 +1,20 @@
 import { String, Text } from 'oak-domain/lib/types/DataType';
 import { EntityShape } from 'oak-domain/lib/types/Entity';
-import { QiniuConfig } from '../types/Config';
+export declare type QiniuConfig = {
+    accessKey: string;
+    secretKey: string;
+    uploadHost: string;
+    liveHost?: string;
+    puhlishDomain?: string;
+    playDomain?: string;
+    playBackDomain?: string;
+    hub?: string;
+    publisthKey?: string;
+    playKey?: string;
+    bucket: string;
+    domain: string;
+    protocol: string | string[];
+};
 export declare type PlatformConfig = {
     Cos?: {
         qiniu?: QiniuConfig;
--- lib/entities/System.d.ts
+++ lib/entities/System.d.ts
@@ -1,7 +1,21 @@
 import { String, Boolean, Text } from 'oak-domain/lib/types/DataType';
 import { EntityShape } from 'oak-domain/lib/types/Entity';
-import { QiniuConfig } from '../types/Config';
 import { Schema as Platform } from './Platform';
+export declare type QiniuConfig = {
+    accessKey: string;
+    secretKey: string;
+    uploadHost: string;
+    liveHost?: string;
+    puhlishDomain?: string;
+    playDomain?: string;
+    playBackDomain?: string;
+    hub?: string;
+    publisthKey?: string;
+    playKey?: string;
+    bucket: string;
+    domain: string;
+    protocol: string | string[];
+};
 export declare type SystemConfig = {
     Cos?: {
         qiniu?: QiniuConfig;
