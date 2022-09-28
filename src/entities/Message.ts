28ce13c0a23f9cab8c41e5ae4970a386b2078bf2
wenjiarui <wenjiarui1@126.com>
2022-09-28 10:14:56 +0800

    message

---
+++ src/entities/Message.ts
@@ -0,0 +1,67 @@
+import { String, Int, Text, Image } from 'oak-domain/lib/types/DataType';
+import { Schema as User } from './User';
+import { Schema as System } from './System';
+import { EntityShape } from 'oak-domain/lib/types/Entity';
+import { LocaleDef } from 'oak-domain/lib/types/Locale';
+import { Index, ActionDef } from 'oak-domain/lib/types';
+
+export interface Schema extends EntityShape {
+    user: User;
+    system: System;
+    type: 'adminNotification';
+    weight: 'high' | 'medium' | 'low' | 'data';
+    props: Object;
+    data: Object;
+    params: Object;
+};
+
+type IAction = 'succeed' | 'fail';
+type IState = 'sending' | 'success' | 'failure';
+
+type Action = IAction;
+
+const IActionDef: ActionDef<IAction, IState> = {
+    stm: {
+        succeed: ['sending', 'success'],
+        fail: ['sending', 'failure'],
+    },
+    is: 'sending',
+};
+
+const locale: LocaleDef<Schema, Action, '', {
+    iState: IState;
+    weight: Schema['weight'],
+    type: Schema['type'],
+}> = {
+    zh_CN: {
+        attr: {
+            user: '关联用户',
+            system: '系统',
+            type: '消息类型',
+            weight: '优先级',
+            iState: '状态',
+            props: '属性',
+            params: '渠道定制参数',
+            data: '透传数据'
+        },
+        action: {
+            succeed: '成功',
+            fail: '失败',
+        },
+        v: {
+            iState: {
+                sending: '发送中',
+                success: '发送成功',
+                failure: '发送失败',
+            },
+            weight: {
+                high: '高',
+                medium: '中',
+                low: '低',
+            },
+            type: {
+                adminNotification: '管理员通知'
+            }
+        }
+    },
+};
---
+++ src/entities/MessageSent.ts
@@ -0,0 +1,60 @@
+import { String, Int, Text, Image } from 'oak-domain/lib/types/DataType';
+import { Schema as Message } from './Message';
+import { EntityShape } from 'oak-domain/lib/types/Entity';
+import { LocaleDef } from 'oak-domain/lib/types/Locale';
+import { Index, ActionDef } from 'oak-domain/lib/types';
+
+export interface Schema extends EntityShape {
+    channel: 'public' | 'jPush' | 'jim' | 'mp' | 'gsm',
+    data: Object,
+    message: Message,
+    data1: Object,
+    data2: Object,
+};
+
+type IAction = 'succeed' | 'fail';
+type IState = 'sending' | 'success' | 'failure';
+
+type Action = IAction;
+
+const IActionDef: ActionDef<IAction, IState> = {
+    stm: {
+        succeed: ['sending', 'success'],
+        fail: ['sending', 'failure'],
+    },
+    is: 'sending',
+};
+
+const locale: LocaleDef<Schema, Action, '', {
+    iState: IState;
+    channel: Schema['channel']
+}> = {
+    zh_CN: {
+        attr: {
+            channel: '消息渠道',
+            data: '消息数据',
+            message: '消息',
+            data1: '数据1',
+            data2: '数据2',
+            iState: '状态',
+        },
+        action: {
+            succeed: '成功',
+            fail: '失败',
+        },
+        v: {
+            iState: {
+                sending: '发送中',
+                success: '发送成功',
+                failure: '发送失败',
+            },
+            channel: {
+                public: '公众号',
+                jPush: '极光推送',
+                jim: '极光消息',
+                mp: '小程序',
+                gsm: '短信',
+            }
+        }
+    },
+};
