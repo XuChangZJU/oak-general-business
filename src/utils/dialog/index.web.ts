f8455c7a388de18f8e690a39d31612eedf4e4d03
wkj <278599135@qq.com>
2022-10-18 14:46:37 +0800

    business 样式文件 调整

--- src/utils/dialog/index.web.ts
+++ src/utils/dialog/index.web.ts
@@ -1,16 +1,3 @@
-//@ts-ignore
-// const { getBrowserWidth } = require('oak-frontend-base/lib/platforms/web/utils/width');
-
-// if (getBrowserWidth() === 'xs') {
-//     const Dialog = require('tdesign-mobile-react').Dialog;
-//     module.exports = Dialog;
-//     module.exports.default = Dialog;
-// } else {
-//     const DialogPlugin = require('tdesign-react').DialogPlugin;
-//     module.exports = DialogPlugin;
-//     module.exports.default = DialogPlugin;
-// }
-
 import { DialogPlugin } from 'tdesign-react';
 
 export default DialogPlugin;
--- src/utils/message/index.web.ts
+++ src/utils/message/index.web.ts
@@ -1,16 +1,3 @@
-//@ts-ignore
-// const { getBrowserWidth } = require('oak-frontend-base/lib/platforms/web/utils/width');
-
-// if (getBrowserWidth() === 'xs') {
-//     const Message = require('tdesign-mobile-react').Message;
-//     module.exports = Message;
-//     module.exports.default = Message;
-// } else {
-//     const MessagePlugin = require('tdesign-react').MessagePlugin;
-//     module.exports = MessagePlugin;
-//     module.exports.default = MessagePlugin;
-// }
-
 import { MessagePlugin } from 'tdesign-react';
 
 export default MessagePlugin;
