f03beec741222fae55a02a6a483dd3eb02c27d2a
wkj <278599135@qq.com>
2022-10-29 17:24:04 +0800

    lib

---
+++ lib/components/common/weChatLoginQrCode/index.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+import './index.less';
+interface QrCodeProps {
+    id?: string;
+    appId: string;
+    scope: string;
+    redirectUri: string;
+    state: string;
+    style?: string;
+    href?: string;
+}
+declare function QrCode(props: QrCodeProps): JSX.Element;
+export default QrCode;
