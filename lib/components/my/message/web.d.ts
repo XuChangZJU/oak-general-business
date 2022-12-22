f7d861878e9aaf642427ebc24d7b2c03cf7a5682
wkj <278599135@qq.com>
2022-12-22 13:35:57 +0800

    my/message 增加 自定义事件及style

--- lib/components/my/message/web.d.ts
+++ lib/components/my/message/web.d.ts
@@ -1,7 +1,11 @@
+import React from 'react';
 import { WebComponentProps } from 'oak-frontend-base';
 import { EntityDict } from '../../../general-app-domain';
 export default function Render(props: WebComponentProps<EntityDict, 'user', false, {
     count?: number;
+    onClick?: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+    className?: string;
+    style?: React.CSSProperties;
 }, {
     goMessageList: () => void;
 }>): JSX.Element;
--- lib/components/my/message/web.pc.d.ts
+++ lib/components/my/message/web.pc.d.ts
@@ -1,7 +1,13 @@
+import React from 'react';
 import { WebComponentProps } from 'oak-frontend-base';
 import { EntityDict } from '../../../general-app-domain';
 export default function Render(props: WebComponentProps<EntityDict, 'user', false, {
     count?: number;
+    onClick: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+    className?: string;
+    style?: React.CSSProperties;
+    buttonStyle?: React.CSSProperties;
+    buttonClassName?: string;
 }, {
     goMessageList: () => void;
 }>): JSX.Element;
