7c2aa0da9604be00878892af13674979dbe68369
wkj <278599135@qq.com>
2023-02-07 10:30:35 +0800

    lib

--- lib/components/extraFile/avatar/web.d.ts
+++ lib/components/extraFile/avatar/web.d.ts
@@ -1,28 +1,9 @@
-/// <reference types="react" />
-import { UploadFile } from 'antd';
 import { WebComponentProps } from 'oak-frontend-base';
 import { EntityDict } from '../../../general-app-domain';
-declare type Theme = 'file' | 'image' | 'file-flow' | 'image-flow' | 'custom';
 export default function render(props: WebComponentProps<EntityDict, 'extraFile', true, {
-    accept?: string;
-    maxNumber?: number;
-    multiple?: boolean;
-    draggable?: boolean;
-    theme?: Theme;
-    tips?: string;
-    beforeUpload?: (file: File) => Promise<boolean>;
-    disabled?: boolean;
     style?: Record<string, string>;
     className?: string;
-    directory?: boolean;
-    onPreview?: (file: UploadFile<any>) => void;
-    onDownload?: (file: UploadFile<any>) => void;
-    showUploadList?: boolean;
-    children?: JSX.Element;
-    files?: EntityDict['extraFile']['OpSchema'][];
-    disableInsert?: boolean;
+    avatarUrl?: string;
 }, {
-    onPickByWeb: (files: UploadFile[], callback?: (file: any, status: string) => void) => void;
-    onDeleteByWeb: (file: UploadFile) => void;
+    onPickByWeb: (files: File[]) => void;
 }>): JSX.Element;
-export {};
