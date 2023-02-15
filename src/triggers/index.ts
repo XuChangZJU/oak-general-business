33be59a5ba42cb4dd6ceda072e4e01c9b21c6bbd
Xc <chinawraith@163.com>
2023-02-15 17:45:23 +0800

    注入了mesage和notification的trigger

--- src/triggers/index.ts
+++ src/triggers/index.ts
@@ -2,5 +2,14 @@ import addressTriggers from './address';
 import userTriggers from './user';
 import userEntityGrantTriggers from './userEntityGrant';
 import wechatQrCodeTriggers from './wechatQrCode';
+import messageTriggers from './message';
+import notificationTriggers from './notification';
 
-export default [...addressTriggers, ...userTriggers, ...userEntityGrantTriggers, ...wechatQrCodeTriggers];
+export default [
+    ...addressTriggers,
+    ...userTriggers,
+    ...userEntityGrantTriggers,
+    ...wechatQrCodeTriggers,
+    ...messageTriggers,
+    ...notificationTriggers,
+];
