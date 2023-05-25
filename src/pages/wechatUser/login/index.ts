3db058a324614b0f52c6e1a3c21e92eabb1ef707
梁朝伟 <2211960668@qq.com>
2023-05-25 11:20:53 +0800

    微信公众号授权登录

--- src/pages/wechatUser/login/index.ts
+++ src/pages/wechatUser/login/index.ts
@@ -1,5 +1,4 @@
-import URL from 'url';
-
+import { Url } from "url";
 export default OakComponent({
     isList: false,
     data: {
@@ -20,7 +19,7 @@ export default OakComponent({
                 loading: true,
             });
             const { features } = this;
-            const token = await features.token.getToken(true);
+            const token = features.token.getToken(true);
             // 解析url
             let url = decodeURIComponent(window.location.href);
             if (this.strCharPosition(url, '?') > 1) {
@@ -31,12 +30,13 @@ export default OakComponent({
                 }
                 url = startStr + endStr;
             }
-            const urlObj = URL.parse(url, true);
+            const urlObj = new URL(url);
             //console.warn('urlObj: '+ JSON.stringify(urlObj))
             //格式 xx?code=xx&state=/xx/xx?d=xx
-            const query = urlObj?.query;
-            const code = query?.code;
-            const state = query?.state;
+            const code = urlObj.searchParams.get('code');
+            const state = urlObj.searchParams.get('state') as string;
+            const wechatLoginId = urlObj.searchParams.get('wechatLoginId') as string;
+
             if (!code) {
                 this.setState({
                     error: '缺少code参数',
@@ -56,7 +56,7 @@ export default OakComponent({
                 console.log('token不存在或失效');
                 try {
                     // web微信扫码跟公众号授权
-                    await features.token.loginWechat(code as string);
+                    await features.token.loginWechat(code as string, { wechatLoginId });
                     this.setState({
                         loading: false,
                     });
@@ -80,24 +80,25 @@ export default OakComponent({
                 });
                 return;
             }
-            const stateObj = URL.parse(
-                decodeURIComponent(state as string),
-                true
-            );
+            const { URL } = require('url');
+            const stateObj = new URL(decodeURIComponent(state as string));
             // console.log('stateObj' + JSON.stringify(stateObj));
-            const stateQuery = stateObj?.query;
             const pathname = stateObj?.pathname;
+            const backUrl = stateObj.searchParams.get('backUrl');
+            const isGoBack = stateObj.searchParams.get('isGoBack');
 
-            if (stateQuery?.backUrl) {
+            const urlSearchParams = new URLSearchParams(stateObj.search);
+            const stateQuery = Object.fromEntries(urlSearchParams.entries());
+            if (backUrl) {
                 // todo 现在不存在跨域名登录 不需要使用window.location.replace
                 // window.location.replace(stateQuery?.backUrl as string);
                 this.redirectTo({
-                    url: stateQuery?.backUrl as string,
+                    url: backUrl as string,
                 });
                 return;
             }
             // 如果stateQuery存在isGoBack为true 返回上一页
-            if (stateQuery?.isGoBack) {
+            if (isGoBack) {
                 this.navigateBack(2);
                 return;
             }
