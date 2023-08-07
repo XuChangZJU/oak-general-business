e90c05a0e70b15bdd5e8d7114134859effd44ee9
wkj <278599135@qq.com>
2023-08-07 17:46:33 +0800

    messageTypeTemplateId select free

--- src/config/relation.ts
+++ src/config/relation.ts
@@ -1,7 +1,7 @@
 
 // 此对象所标识的外键关系不参与relation的路径判定，以减少relation的路径数量
 export const IgnoredForeignKeyMap = {
-    system: ['park'],
+    //system: ['park'],
     messageSystem: ['message'],
     notification: ['messsageSystem'],
 };
