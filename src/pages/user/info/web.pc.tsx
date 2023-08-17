d47205a5e005a2c41ffea298bcf5082880f35e9c
wenjiarui <wenjiarui1@126.com>
2023-08-17 17:41:05 +0800

    个人信息页面修改

--- src/pages/user/info/web.pc.tsx
+++ src/pages/user/info/web.pc.tsx
@@ -9,6 +9,7 @@ import {
     Form,
     Typography,
     Modal,
+    Descriptions
 } from 'antd';
 import dayjs from 'dayjs';
 import { WebComponentProps } from 'oak-frontend-base';
@@ -80,6 +81,7 @@ export default function Render(
     return (
         <PageHeader title="个人设置" showBack={showBack}>
             <div className={Style.container}>
+                <Descriptions title={'基本信息'}></Descriptions>
                 <Form
                     labelCol={{ xs: { span: 4 }, md: { span: 6 } }}
                     wrapperCol={{ xs: { span: 16 }, md: { span: 12 } }}
@@ -213,6 +215,7 @@ export default function Render(
             </div>
             <div style={{ marginTop: '10px' }}></div>
             <div className={Style.container}>
+                <Descriptions title={'安全信息'}></Descriptions>
                 <Form
                     labelCol={{ xs: { span: 4 }, md: { span: 6 } }}
                     wrapperCol={{ xs: { span: 16 }, md: { span: 12 } }}
@@ -236,7 +239,7 @@ export default function Render(
                             </Space>
                         </>
                     </Form.Item>
-                    {process.env.NODE_ENV === 'development' && (<Form.Item label={t('user:attr.password')}>
+                    <Form.Item label={t('user:attr.password')}>
                         <>
                             <Space>
                                 <Typography>{'********'}</Typography>
@@ -251,7 +254,7 @@ export default function Render(
                                 </Button>
                             </Space>
                         </>
-                    </Form.Item>)}
+                    </Form.Item>
                     {process.env.NODE_ENV === 'development' && (
                         <Form.Item label="微信帐号">
                             <>
