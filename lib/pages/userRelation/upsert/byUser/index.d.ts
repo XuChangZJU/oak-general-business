bee22312989e9b5cb42918f6d57d915795479fa6
wkj <278599135@qq.com>
2023-08-22 12:01:39 +0800

    userRelation 移动components

--- lib/pages/userRelation/upsert/byUser/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { EntityDict } from '../../../../oak-app-domain';
-import { ReactComponentProps } from 'oak-frontend-base/lib/types/Page';
-import { EntityDict as BaseEntityDict } from 'oak-domain/lib/types/Entity';
-declare const _default: <ED2 extends EntityDict & BaseEntityDict, T2 extends keyof ED2>(props: ReactComponentProps<ED2, T2, true, {
-    entity: keyof ED2;
-    entityId: string;
-}>) => React.ReactElement;
-export default _default;
