6be20abd3df6b97daf80985b4660b25836acee46
梁朝伟 <2211960668@qq.com>
2023-08-24 19:09:37 +0800

    add: message/detail message/list

---
+++ lib/components/message/list/web.d.ts
@@ -0,0 +1,7 @@
+import { WebComponentProps } from 'oak-frontend-base';
+import { EntityDict } from '../../../oak-app-domain';
+export default function Render(props: WebComponentProps<EntityDict, 'message', true, {
+    messages: EntityDict['message']['Schema'][];
+}, {
+    goDetailById: (id: string) => void;
+}>): JSX.Element;
---
+++ src/components/message/detail/web.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+    Card,
+    Button,
+} from 'antd';
+import { EntityDict } from '../../../oak-app-domain';
+import { WebComponentProps } from 'oak-frontend-base';
+import Style from './mobile.module.less';
+
+
+export default function Render(
+    props: WebComponentProps<
+        EntityDict,
+        'message',
+        false,
+        {
+            title: string;
+            content: string;
+            id: string;
+            $$createAt$$: number;
+            type: string;
+            visitState: EntityDict['message']['Schema']['visitState'];
+            router: EntityDict['message']['Schema']['router'];
+        },
+        {
+            goPage: () => void;
+        }
+    >
+) {
+    const { data, methods } = props;
+    const { title, content, router } = data;
+    const { t, goPage } = methods;
+    const pathname = router?.pathname;
+
+    return (
+        <div className={Style.container}>
+            <h1 className={Style.title}>{title}</h1>
+            <div className={Style.content}>{content}</div>
+
+            {pathname && (
+                <Button
+                    className={Style.btn}
+                    block
+                    type="primary"
+                    onClick={() => {
+                        goPage();
+                    }}
+                >
+                    前往
+                </Button>
+            )}
+        </div>
+    );
+}
