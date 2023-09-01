72e41e4e889d7ea8884b7ba8cf4665f46c1ebc1b
wkj <278599135@qq.com>
2023-09-01 19:26:04 +0800

    删除无用代码

--- src/general-app-domain/UserEntityGrant/Action.ts
+++
@@ -1,6 +0,0 @@
-import { ActionDef } from "oak-domain/lib/types/Action";
-import { GenericAction, AppendOnlyAction, ReadOnlyAction, ExcludeUpdateAction, ExcludeRemoveAction, RelationAction } from "oak-domain/lib/actions/action";
-export type ParticularAction = 'confirm';
-export type Action = GenericAction | ParticularAction | string;
-export const actions = ["count", "stat", "download", "select", "aggregate", "create", "remove", "update", "confirm"];
-export const ActionDefDict = {};
