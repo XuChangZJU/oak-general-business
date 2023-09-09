e79e7aec008ecc1795531e6538439eec63bd1dc6
wkj <278599135@qq.com>
2023-09-09 09:54:07 +0800

    build

---
+++ lib/types/Projection.d.ts
@@ -0,0 +1,4 @@
+import { EntityDict } from '../oak-app-domain';
+export declare const userProjection: EntityDict['user']['Selection']['data'];
+export declare const tokenProjection: EntityDict['token']['Selection']['data'];
+export declare const applicationProjection: EntityDict['application']['Selection']['data'];
