2eac154a1801bd50b8f4a38f3ac0869933404552
Xc@centOs <chinawraith@163.com>
2023-11-11 21:33:41 +0800

    address的过期checker

--- es/checkers/address.js
+++ es/checkers/address.js
@@ -1,39 +1,11 @@
 import { isMobile } from 'oak-domain/lib/utils/validator';
 import { OakInputIllegalException } from "oak-domain/lib/types";
-import { checkAttributesNotNull } from 'oak-domain/lib/utils/validator';
 const checkers = [
-    {
-        type: 'data',
-        action: 'create',
-        entity: 'address',
-        checker: (data) => {
-            if (data instanceof Array) {
-                data.forEach(ele => {
-                    checkAttributesNotNull('address', ele, ['name', 'detail', 'phone' /* , 'areaId' */]);
-                    if (!isMobile(ele.phone)) {
-                        throw new OakInputIllegalException('address', ['phone'], '手机号非法');
-                    }
-                });
-            }
-            else {
-                checkAttributesNotNull('address', data, ['name', 'detail', 'phone' /* , 'areaId' */]);
-                if (!isMobile(data.phone)) {
-                    throw new OakInputIllegalException('address', ['phone'], '手机号非法');
-                }
-            }
-        },
-    },
     {
         type: 'data',
         action: 'update',
         entity: 'address',
         checker: (data) => {
-            if (data.name === '') {
-                throw new OakInputIllegalException('address', ['name'], '姓名不可为空');
-            }
-            if (data.detail === '') {
-                throw new OakInputIllegalException('address', ['name'], '详细地址不可为空');
-            }
             if (data.hasOwnProperty('phone') && !isMobile(data.phone)) {
                 throw new OakInputIllegalException('address', ['phone'], '手机号非法');
             }
--- src/checkers/address.ts
+++ src/checkers/address.ts
@@ -7,40 +7,10 @@ import { RuntimeCxt } from '../types/RuntimeCxt';
 const checkers: Checker<EntityDict, 'address', RuntimeCxt> [] = [
     {
         type: 'data',
-        action: 'create',
-        entity: 'address',
-        checker: (data) => {
-            if (data instanceof Array) {
-                data.forEach(
-                    ele => {
-                        checkAttributesNotNull('address', ele, ['name', 'detail', 'phone'/* , 'areaId' */]);
-                        if (!isMobile(ele.phone!)) {
-                            throw new OakInputIllegalException('address', ['phone'], '手机号非法');
-                        }
-                    } 
-                );
-            }
-            else {
-                checkAttributesNotNull('address', data, ['name', 'detail', 'phone'/* , 'areaId' */]);
-                if (!isMobile(data.phone!)) {
-                    throw new OakInputIllegalException('address', ['phone'], '手机号非法');
-                }
-            }
-        },
-    },
-    {
-
-        type: 'data',
         action: 'update',
         entity: 'address',
-        checker: (data: EntityDict['address']['Update']['data']) => {
-            if (data.name === '') {
-                throw new OakInputIllegalException('address', ['name'], '姓名不可为空');
-            }
-            if (data.detail === '') {
-                throw new OakInputIllegalException('address', ['name'], '详细地址不可为空');
-            }
-            if (data.hasOwnProperty('phone') && !isMobile(data.phone!)) {
+        checker: (data) => {
+            if (data.hasOwnProperty('phone') && !isMobile((data as EntityDict['address']['Update']['data']).phone!)) {
                 throw new OakInputIllegalException('address', ['phone'], '手机号非法');
             }
             return;
