9068dfc33a8fa325ade957cae0d532e6e4e20c99
wangwenchen <920960949@qq.com>
2024-01-20 10:51:14 +0800

    采集码

--- es/components/parasite/detail/web.js
+++ es/components/parasite/detail/web.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import QrCode from '../../../components/common/qrCode';
-import { Spin, Button, Space, Input, Tooltip } from 'antd';
+import { Spin, Button, Space, Input, Tooltip,Alert } from 'antd';
 import { CopyOutlined } from '@ant-design/icons';
 import copy from 'copy-to-clipboard';
 export default function Render(props) {
@@ -21,7 +21,8 @@ export default function Render(props) {
         }}>
             <div style={{
             maxWidth: 800
-        }}>
+            }}>
+                <Alert message="温馨提示：请扫描二维码或打开链接填写信息！" type="info" />
                 <QrCode url={url} expiresAt={expiresAt}/>
 
                 <Space.Compact block style={{ marginTop: 16 }}>
